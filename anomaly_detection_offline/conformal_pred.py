215790cbeb008b60f036a86d9e449d43525a2f7c
LeonHardt427 <leonhardt427@126.com>
2018-04-04 14:47:45 +0800

    K_fold anomaly

--- anomaly_detection_offline/conformal_pred.py
+++ anomaly_detection_offline/conformal_pred.py
@@ -8,8 +8,10 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 from nonconformist.base import ClassifierAdapter
 from nonconformist.cp import IcpClassifier
@@ -20,29 +22,59 @@ from nonconformist.nc import ClassifierNc, MarginErrFunc
 # -----------------------------------------------------------------------------
 path = os.getcwd()
 
-X = np.loadtxt('x_sample.csv', delimiter=',')
-y = np.loadtxt('y_label.csv', delimiter=',', dtype='int8')
+X = np.loadtxt('dendrobium_x_sample.csv', delimiter=',')
+y = np.loadtxt('dendrobium_y_label.csv', delimiter=',', dtype='int8')
+x_anomaly = np.loadtxt('x_sample_anomaly.csv', delimiter=',')
 
 sc = StandardScaler()
 X = sc.fit_transform(X)
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, shuffle=True)
 
-model = SVC(probability=True)
-# -----------------------------------------------------------------------------
-# Train and calibrate
-# -----------------------------------------------------------------------------
-x_train_sp, x_cal, y_train_sp, y_cal = train_test_split(x_train, y_train, test_size=0.3, shuffle=True)
+correct = []
+anomaly = []
 
-icp = IcpClassifier(ClassifierNc(ClassifierAdapter(model),
-                                 MarginErrFunc()))
-icp.fit(x_train, y_train)
-icp.calibrate(x_cal, y_cal)
+s_fold = StratifiedKFold(n_splits=10, shuffle=True)
+for train_index, test_index in s_fold.split(X, y):
+    X_train, y_train = X[train_index], y[train_index]
+    X_test, y_test = X[test_index], y[test_index]
 
-# -----------------------------------------------------------------------------
-# Predict
-# -----------------------------------------------------------------------------
-prediction = icp.predict(x_test, significance=0.5)
-# print(prediction)
-result = np.sum(prediction, axis=1)
-print(result)
+    lda = LinearDiscriminantAnalysis(n_components=9)
+    X_train_lda = lda.fit_transform(X_train, y_train)
+    X_test_lda = lda.transform(X_test)
+    x_anomaly_lda = lda.transform(x_anomaly)
 
+    x_train, x_cal, y_train, y_cal = train_test_split(X_train_lda,
+                                                      y_train, test_size=0.3, shuffle=False, random_state=1)
+
+    model = KNeighborsClassifier(n_neighbors=5)
+    # -----------------------------------------------------------------------------
+    # Train and calibrate
+    # -----------------------------------------------------------------------------
+
+    icp = IcpClassifier(ClassifierNc(ClassifierAdapter(model)))
+    icp.fit(x_train, y_train)
+    icp.calibrate(x_cal, y_cal)
+
+    # -----------------------------------------------------------------------------
+    # Predict
+    # -----------------------------------------------------------------------------
+    SIG = 0.2
+    prediction = icp.predict(X_test_lda, significance=SIG)
+    result = np.sum(prediction, axis=1)
+    zero_sum_correct = (48 - result.sum(axis=0))/48
+    correct.append(zero_sum_correct)
+    print("the correct prediction")
+    print(result)
+
+    prediction_anomaly = icp.predict(x_anomaly_lda, significance=SIG)
+    result_anomaly = np.sum(prediction_anomaly, axis=1)
+    zero_sum_anomaly = (50 - result_anomaly.sum(axis=0)) / 50
+    anomaly.append(zero_sum_anomaly)
+    print("the anomaly prediction")
+    print(result_anomaly)
+
+print("correct_summary")
+print(correct)
+print(np.mean(correct))
+print("anomaly_summary")
+print(anomaly)
+print(np.mean(anomaly))
