f064dad24a7af1c614b38e104a14d90ff583905c
LeonHardt427 <leonhardt427@126.com>
2018-04-15 19:08:23 +0800

    knn_offline

--- anomaly_detection_offline/CAD_KNN_new.py
+++ anomaly_detection_offline/CAD_KNN_new.py
@@ -15,7 +15,7 @@ def k_neighourdis(x_test, train, k=3):
     :param x_test: numpy
     :param train: numpy
     :param k: int
-    :return alp: float
+    :return alp: []
     """
     neigh = NearestNeighbors(n_neighbors=k, p=2)
     neigh.fit(train)
@@ -27,6 +27,7 @@ def k_neighourdis(x_test, train, k=3):
 
 def cad_knn_new(x_test, y_test, x_train, y_train, n_nei=3):
     """
+    计算p_value（Alpha包含其中）
     :param x_test: array(1, )
     :param y_test: array
     :param x_train: array
@@ -35,22 +36,35 @@ def cad_knn_new(x_test, y_test, x_train, y_train, n_nei=3):
     :return: array( ,1)
     """
     result = []
-    for y_label in y_test:
-        nei_sample = []
+    p_value = []
+    for y_index, y_label in enumerate(y_test):
+        nei_sample = []   # same label train samples
 
         for index, label in enumerate(y_train):
             if label == y_label:
                 nei_sample.append(x_train[index, :])
 
-        test_alph = k_neighourdis(x_test, nei_sample, k=n_nei)
-        test_value = 1
+        test_alphs = k_neighourdis(x_test, nei_sample, k=n_nei)    # all test samples alpha
+        test_alph_temp = np.ones(test_alphs.shape)
+
         for train_sample in nei_sample:
             train_alph_sample = k_neighourdis([train_sample], nei_sample, k=n_nei)
-            if train_alph_sample >= test_alph:
-                test_value += 1
-    # count p_value
-        p_value = [test_value / (np.shape(nei_sample)[0]), y_label]
-        result.append(p_value)
+            for index, test_alph in enumerate(test_alphs):
+                if train_alph_sample >= test_alph:
+                    test_alph_temp[index] += 1
+    # count p_value for one label
+        for num, alpha in enumerate(test_alph_temp):
+            if num == 0:
+                p_value_temp = [alpha / (np.shape(nei_sample)[0])]
+            else:
+                p_value_temp.append((alpha / (np.shape(nei_sample)[0])))
+
+    # summary p_value
+        if y_index == 0:
+            p_value = np.array(p_value_temp).T
+        else:
+            p_value = np.hstack((p_value, np.array(p_value_temp).T))
+
     return result
 
 
@@ -59,14 +73,14 @@ if __name__ == '__main__':
                   [0, 1, 1],
                   [1, 0.5, 1]])
 
-    a_y = [1, 1, 1]
+    a_y = [1, 0, 1]
 
-    # b = np.array([[1, 1, 1],
-    #               [0.5, 1, 1],
-    #               [0.5, 0.5, 1],
-    #               [1, 0, 0]])
-    b = np.array([[0, 0.5, 0.5]])
-    b_test = [1]
+    b = np.array([[1, 1, 1],
+                  [0.5, 1, 1],
+                  [0.5, 0.5, 1],
+                  [1, 0, 0]])
+    # b = np.array([[0, 0.5, 0.5]])
+    b_test = [0, 1]
 
     result = cad_knn_new(b, b_test, a, a_y, n_nei=1)
     print(result)
--- anomaly_detection_offline/anomaly_offline.py
+++ anomaly_detection_offline/anomaly_offline.py
@@ -6,6 +6,7 @@
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, StratifiedKFold
@@ -21,19 +22,27 @@ from nonconformist.nc import ClassifierNc, MarginErrFunc
 # Setup training, calibration and test indices
 # -----------------------------------------------------------------------------
 path = os.getcwd()
+save_path = os.getcwd() + '/ICP_result/'
+if os.path.exists(save_path) is False:
+    os.makedirs(save_path)
 
 X = np.loadtxt('dendrobium_x_sample.csv', delimiter=',')
 y = np.loadtxt('dendrobium_y_label.csv', delimiter=',', dtype='int8')
 x_anomaly = np.loadtxt('x_sample_anomaly.csv', delimiter=',')
 
+signs = np.arange(0.05, 1, 0.05)
+
 sc = StandardScaler()
 X = sc.fit_transform(X)
 
-correct = []
-anomaly = []
+correct_summary = []
+anomaly_summary = []
 
 s_fold = StratifiedKFold(n_splits=10, shuffle=True)
-for train_index, test_index in s_fold.split(X, y):
+for index, (train_index, test_index) in enumerate(s_fold.split(X, y)):
+    correct = []
+    anomaly = []
+
     X_train, y_train = X[train_index], y[train_index]
     X_test, y_test = X[test_index], y[test_index]
 
@@ -45,7 +54,7 @@ for train_index, test_index in s_fold.split(X, y):
     x_train, x_cal, y_train, y_cal = train_test_split(X_train_lda,
                                                       y_train, test_size=0.3, shuffle=False, random_state=1)
 
-    model = KNeighborsClassifier(n_neighbors=5)
+    model = KNeighborsClassifier(n_neighbors=6)
     # -----------------------------------------------------------------------------
     # Train and calibrate
     # -----------------------------------------------------------------------------
@@ -57,24 +66,52 @@ for train_index, test_index in s_fold.split(X, y):
     # -----------------------------------------------------------------------------
     # Predict
     # -----------------------------------------------------------------------------
-    SIG = 0.2
-    prediction = icp.predict(X_test_lda, significance=SIG)
-    result = np.sum(prediction, axis=1)
-    zero_sum_correct = (48 - result.sum(axis=0))/48
-    correct.append(zero_sum_correct)
-    print("the correct prediction")
-    print(result)
+    for sig in signs:
+        print("sig=" + str(sig))
+        SIG = sig
+        prediction = icp.predict(X_test_lda, significance=SIG)
+        result = np.sum(prediction, axis=1)
+        result = result > 0
+        zero_sum_correct = (result.sum(axis=0))/48
+        correct.append(zero_sum_correct)
+        print("the correct prediction")
+        print(result)
 
-    prediction_anomaly = icp.predict(x_anomaly_lda, significance=SIG)
-    result_anomaly = np.sum(prediction_anomaly, axis=1)
-    zero_sum_anomaly = (50 - result_anomaly.sum(axis=0)) / 50
-    anomaly.append(zero_sum_anomaly)
-    print("the anomaly prediction")
-    print(result_anomaly)
+        prediction_anomaly = icp.predict(x_anomaly_lda, significance=SIG)
+        result_anomaly = np.sum(prediction_anomaly, axis=1)
+        result_anomaly = result_anomaly > 0
+        zero_sum_anomaly = (result_anomaly.sum(axis=0)) / 50
+        anomaly.append(zero_sum_anomaly)
+        print("the anomaly prediction")
+        print(result_anomaly)
 
-print("correct_summary")
-print(correct)
-print(np.mean(correct))
-print("anomaly_summary")
-print(anomaly)
-print(np.mean(anomaly))
+    if index == 0:
+        correct_summary = correct
+        anomaly_summary = anomaly
+    else:
+        correct_summary = np.vstack((correct_summary, correct))
+        anomaly_summary = np.vstack((anomaly_summary, anomaly))
+
+np.savetxt(save_path+'ICP_prediction.txt', correct_summary, delimiter=',')
+np.savetxt(save_path+'ICP_anomaly.txt', anomaly_summary, delimiter=',')
+
+correct_plt = np.mean(correct_summary, axis=0)
+anomaly_plt = np.mean(anomaly_summary, axis=0)
+
+fig = plt.figure(figsize=(12, 16))
+ax1 = plt.subplot(1, 1, 1)
+
+ax1.plot(signs, correct_plt, linestyle='-', color='blue', label='normal_sample')
+ax1.plot(signs, anomaly_plt, linestyle='-', color='red', label='anomaly_sample')
+ax1.set_xlim(0, 1)
+ax1.set_ylim(0, 1)
+ax1.legend(loc='best')
+ax1.set_title('ICP_KNN anomaly detection')
+ax1.set_xlabel("Significance", fontsize=10, fontweight='bold')
+ax1.set_ylabel("Average count", fontsize=10, fontweight='bold')
+
+plt.show()
+
+
+
+
